21fa1c4ef2abcd707580d71d1cd0e3872958b976
cohaolee <cohaolee@163.com>
2017-06-06 11:27:19 +0800

    代码注释

--- src/main/java/redis/clients/jedis/params/set/SetParams.java
+++ src/main/java/redis/clients/jedis/params/set/SetParams.java
@@ -7,6 +7,12 @@ import redis.clients.util.SafeEncoder;
 
 public class SetParams extends Params {
 
+//SET key value [EX seconds] [PX milliseconds] [NX|XX]
+//  EX second ：设置键的过期时间为 second 秒。 SET key value EX second 效果等同于 SETEX key second value 。
+//  PX millisecond ：设置键的过期时间为 millisecond 毫秒。 SET key value PX millisecond 效果等同于 PSETEX key millisecond value 。
+//  NX ：只在键不存在时，才对键进行设置操作。 SET key value NX 效果等同于 SETNX key value 。
+//  XX ：只在键已经存在时，才对键进行设置操作。
+
   private static final String XX = "xx";
   private static final String NX = "nx";
   private static final String PX = "px";
